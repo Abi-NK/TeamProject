a49e46db988d01d0347b971a2fda1f915a76320a
25640857+kkablan <25640857+kkablan@users.noreply.github.com>
2018-01-18 17:13:59 +0000

    I have added a custom, basic, website that shows text. This should be replaced with returning JSON.

--- server/serverController/views.py
+++ server/serverController/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
+
+    if request.method == 'GET':
+    	return HttpResponse("Menu:")
     return HttpResponse("Hello, world. You're at the polls index.")
