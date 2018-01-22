7b4a8fb77357d17a9746f249277b53ca5d83593d
Henry Gilbert <henrygilbert15@live.co.uk>
2018-01-22 16:25:38 +0000

    Refactor index view to use the render shortcut

--- server/customer/views.py
+++ server/customer/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Menu
-from django.template import loader
 
 
 # A view is a simple function that returns html.
@@ -11,9 +9,6 @@ def index(request):
     # the table with different queries.
     all_menu = Menu.objects.all()
 
-    # Loads the html file as a template.
-    template = loader.get_template('customer/menu.html')
-
     # Context gets sent straight into the html file, this is needed because it's
     # better to keep all the html code separated from all the python logic.
     # There should never be raw html inside the python files.
@@ -21,8 +16,8 @@ def index(request):
         'all_menu': all_menu,
     }
 
-    # Returns the template as a response.
-    return HttpResponse(template.render(context, request))
+    # Returns the rendered template as a response.
+    return render(request, 'customer/menu.html', context)
 
 
 def detail(request, menu_id):
