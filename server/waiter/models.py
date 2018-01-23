f5a2169d73e65ecf0589fc207bf3d1f7c8ac5d45
25640857+kkablan <25640857+kkablan@users.noreply.github.com>
2018-01-23 21:35:42 +0000

    I have complected some of the waiter backend. The waiter now checks, every time the page loads, if the status for the order has changed then it will be printed.
    As of yet I am not sure how to return this data, so for now it is printed on the local server.
    
    apps.py:
    I have renamed the class, as requested.
    
    settings.py:
    changed the name to be the new class, see apps.py. and added waiterconfig
    
    urls.py:
    added basic index for testing waiter class
    
    I have also refactored some code

--- server/waiter/models.py
+++ server/waiter/models.py
@@ -6,3 +6,9 @@ class Order(models.Model):
 
     orderNumber = models.IntegerField()
     orderStatus = models.BooleanField(default=False)
+
+    def __str__(self):
+        if self.orderStatus:
+            return str(self.orderNumber) + ' - ' + 'READY'
+        else:
+            return str(self.orderNumber) + ' - ' + 'Not ready'
--- server/waiter/views.py
+++ server/waiter/views.py
@@ -3,18 +3,32 @@ from django.http import JsonResponse
 from django.http import HttpResponse
 
 
+# list of orders that are ready is updated every time the page is accessed (refreshed)
+
 def index(request):
-    orderupdate()
-    return HttpResponse()
+    readyorders = orderupdate()
+    context = {
+        'readyorders': readyorders,
+    }
+    # can use the readorders within the HTML in a template to access a list of ready orders
+    return HttpResponse(readyorders)
 
 
-#This method is used to check the db every 30 seconds
+# This method is used to check the db every 30 seconds
 # to see if the order has updated.
 def orderupdate():
     print ("-----checking for order update-----")
     readyorders = Order.objects.filter(orderStatus=True)
-    print (readyorders)
+    try:
+        for curIt in readyorders:
+            # Note -- add a method here to send the readyorders object i.e. through JSON.
+            print (str(curIt.orderNumber) + ' -> ' + str(curIt.orderStatus))
+    except:
+        print("error printing")
+    return readyorders
+
 
 #def takeOrder():
-    #method to take an order this involves added a new order to the tablecd
+    # method to take an order this involves added a new order to the table
+
 
