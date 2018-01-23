0d03f50442001be7f334cb01209fefc9b78916c3
Tom McGregor <zdac099@live.rhul.ac.uk>
2018-01-23 18:47:19 +0000

    Finished tutorial to an extent that I am happy. I will now begin work on the database

--- DT2/tutorial/music/urls.py
+++ DT2/tutorial/music/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views # from current directory
 
 urlpatterns = [
+    # /music/
     path('', views.index, name='index'),
+    # /music/123/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- DT2/tutorial/music/views.py
+++ DT2/tutorial/music/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>This is the music 'app' homepage</h1>")
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+    return HttpResponse(html)
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for album_id " + str(album_id) + "</h2>")
