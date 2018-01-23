568beb892297dfd21ee41fe4118f63e81a6feb69
Henry Gilbert <henrygilbert15@live.co.uk>
2018-01-23 18:50:33 +0000

    Add clicked items to object in JavaScript

--- server/customer/static/customer/js/menu.js
+++ server/customer/static/customer/js/menu.js
@@ -1,5 +1,28 @@
+// the object of items ordered and quantity
 var order = {};
+// used locally for displaying order info, should never be returned to the server
+var itemNames = {};
+var itemPrices = {};
 
-function addToOrder(menuItemID){
-  console.log(menuItemID);
+// called by buttons on menu items, ads them to the order object
+function addToOrder(menuItemID, menuItemName, menuItemPrice) {
+  // increment the counter for that menu item, or create it
+  if (order.hasOwnProperty(menuItemID)) {
+    order[menuItemID] += 1;
+  } else {
+    order[menuItemID] = 1;
+  }
+
+  // update the name and price of the menu item for order displaying
+  itemNames[menuItemID] = menuItemName;
+  itemPrices[menuItemID] = menuItemPrice;
+
+  console.log(order);
+  console.log(itemNames);
+  console.log(itemPrices);
+  calculateTotal();
 }
+
+$(document).ready(function() {
+  $("#total-price").text("£0.00");
+});
