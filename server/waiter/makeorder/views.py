8c1c09712f407649f6362905712f656d81b70b3e
25640857+kkablan <25640857+kkablan@users.noreply.github.com>
2018-01-24 20:36:57 +0000

    I have moved the takeorder method into its own app so it can be accsessed by .../waiter/takeorder -> this is activated by a button.

--- server/waiter/makeorder/views.py
+++ server/waiter/makeorder/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here..
+def index(request):
+    try:
+        takeorder(request)
+        print("order taken")
+        return HttpResponse("order taken")
+    except:
+        print("problem taking order")
+        return HttpResponse("Error taking order")
+
+
+
+def takeorder(request):
+    # method to take an order this involves added a new order to the table.
+    #if request.HttpRequest.POST:
+        if request.method == 'POST':
+            print 'Raw Data: "%s"' % request.body
+            parsed_json = req.body
+            # should be equivalent to:
+            # parsed_json = {"1": 3, "2": 6, "3": 1}
+            parsed_json["1"] = 3
+            # or
+            for key, value in parsed_json:
+                # key is the primary key of the menu item
+                # value is quantity ordered
+        return HttpResponse("OK")
+
+
+    # request.POST.get(data to get) data i.e. orderNumber
+
+    # alternative
+    # data = simplejson.loads(request.POST['data'])
--- server/waiter/views.py
+++ server/waiter/views.py
@@ -32,25 +32,4 @@ def orderupdate():
     return readyorders
 
 
-def takeorder(request):
-    # method to take an order this involves added a new order to the table.
-    #if request.HttpRequest.POST:
-        if request.method == 'POST':
-            print 'Raw Data: "%s"' % request.body
-            parsed_json = req.body
-            # should be equivalent to:
-            # parsed_json = {"1": 3, "2": 6, "3": 1}
-            parsed_json["1"] = 3
-            # or
-            for key, value in parsed_json:
-                # key is the primary key of the menu item
-                # value is quantity ordered
-        return HttpResponse("OK")
-
-
-    # request.POST.get(data to get) data i.e. orderNumber
-
-    # alternative
-    # data = simplejson.loads(request.POST['data'])
-
 
