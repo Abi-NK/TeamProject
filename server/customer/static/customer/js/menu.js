296ed9e247607717d6baf6a10a8913430fb945db
Henry Gilbert <henrygilbert15@live.co.uk>
2018-01-24 11:26:12 +0000

    Add "add to order" buttons adding menu item to order object

--- server/customer/static/customer/js/menu.js
+++ server/customer/static/customer/js/menu.js
@@ -3,6 +3,23 @@ var order = {};
 // used locally for displaying order info, should never be returned to the server
 var itemNames = {};
 var itemPrices = {};
+var stringTotal = "total"
+
+// returns the sum of all items in the order times their quantity
+function calculateTotal(){
+  var total = 0;
+  for (const [item, quantity] of Object.entries(order)){
+    total += (itemPrices[item] * quantity);
+  }
+  return total;
+}
+
+// calculates the total, stores the 2dp string version, updates on screen total
+function updateTotal(){
+  var total = calculateTotal()
+  stringTotal = Number.parseFloat(total).toFixed(2);
+  $("#total-price").text("£" + stringTotal);
+}
 
 // called by buttons on menu items, ads them to the order object
 function addToOrder(menuItemID, menuItemName, menuItemPrice) {
@@ -17,12 +34,10 @@ function addToOrder(menuItemID, menuItemName, menuItemPrice) {
   itemNames[menuItemID] = menuItemName;
   itemPrices[menuItemID] = menuItemPrice;
 
-  console.log(order);
-  console.log(itemNames);
-  console.log(itemPrices);
-  calculateTotal();
+  updateTotal();
+  console.log(menuItemName + " added to order, new total is £" + stringTotal);
 }
 
 $(document).ready(function() {
-  $("#total-price").text("£0.00");
+  updateTotal();
 });
