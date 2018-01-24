3ff52dd87f440f6a3188c71cbc48f4c3d32f2b2a
25640857+kkablan <25640857+kkablan@users.noreply.github.com>
2018-01-24 22:21:44 +0000

    I will admit, its all broken.
    Admin page no longer works
    
    orderstatus has a problem where it is looking for waiter.order.customer_name for some reason.

--- server/waiter/orderstatus/views.py
+++ server/waiter/orderstatus/views.py
@@ -12,18 +12,19 @@ def index(request):
     }
     # can use the readorders within the HTML in a template to access a list of ready orders
     # return HttpResponse(readyorders) # this line returns the http with the list of ready orders
-    return HttpResponse()  # this line returns an empty page (the effects can be seen in the terminal)
+    return HttpResponse(readyorders)  # this line returns an empty page (the effects can be seen in the terminal)
 
 
 # This method is used to check the db every 30 seconds
 # to see if the order has updated.
 def orderupdate():
     print ("-----checking for order update-----")
-    readyorders = Order.objects.filter(order_complete=True)
+    # readyorders = Order.objects.filter(order_complete=True)
+    readyorders = Order.objects.all
     try:
         for curIt in readyorders:
             # Note -- add a method here to send the readyorders object i.e. through JSON.
-            print (str(curIt[0]) + ' -> ' + 'READY')  # str(curIt.orderStatus))
+            print (str(curIt.customer_name) + ' -> ' + 'READY')  # str(curIt.orderStatus))
     except:
         print("error printing")
     return readyorders
