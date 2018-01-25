b1c8fadb4e02e72f9d8773fd8f74b0c239c769f1
Tom McGregor <zdac099@live.rhul.ac.uk>
2018-01-25 13:17:59 +0000

    Start of using index pages for the apps that don't have them - using static pages and template_views, currently uses embedded CSS as can't get links to work for some reason - will change this next Sprint

--- server/server/urls.py
+++ server/server/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
 	path('serverController/', include('serverController.urls')),
     path('customer/', include('customer.urls')),
     path('admin/', admin.site.urls),
