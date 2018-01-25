9914a7950f7f4523f217d626ef0c3869a6e80255
25640857+kkablan <25640857+kkablan@users.noreply.github.com>
2018-01-25 10:02:14 +0000

    I have changed the orderupdate method to return all the orders ready or not from .../waiter/orderstatus

--- server/waiter/orderstatus/views.py
+++ server/waiter/orderstatus/views.py
@@ -21,12 +21,13 @@ def index(request):
 # to see if the order has updated.
 def orderupdate():
     print ("-----checking for order update-----")
-    readyorders = Order.objects.filter(order_complete=True)
+    readyorders = Order.objects.all()
+    realyreadyorders = Order.objects.filter(order_complete=True)
     notreadyorders = Order.objects.filter(order_complete=False)
     # readyorders = Order.objects.all
     # print (Order.objects.all())
     try:
-        for curIt in readyorders:
+        for curIt in realyreadyorders:
             # Note -- add a method here to send the readyorders object i.e. through JSON.
             print (str(curIt.id) + ' -> ' + 'READY ')  # str(curIt.orderStatus))
         for curIt in notreadyorders:
