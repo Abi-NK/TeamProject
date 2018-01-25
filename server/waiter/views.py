4b463227dc52c4fafa3146a98ac76c0b3df2d4f2
Tom McGregor <zdac099@live.rhul.ac.uk>
2018-01-25 20:01:50 +0000

    Added an index page for the waiters' section made with bootstrap again, it's very bare bones at the moment

--- server/waiter/views.py
+++ server/waiter/views.py
@@ -1,11 +1,10 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 # list of orders that are ready is updated every time the page is accessed (refreshed)
 
+
 def index(request):
-    return HttpResponse("This page has the potential to be the best page ever... it's just not there yet")
-
-
+    return render(request, 'waiter_index.html')
 
 
