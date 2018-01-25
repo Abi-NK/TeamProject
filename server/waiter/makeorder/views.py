00b8b143dc1af84272ceb618e7ebcec838b996e8
25640857+kkablan <25640857+kkablan@users.noreply.github.com>
2018-01-25 10:14:10 +0000

    I have changed the make order method to add a new order to the db. this is not working as i am not sure of the format

--- server/waiter/makeorder/views.py
+++ server/waiter/makeorder/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from waiter.models import Order
 
 def index(request):
     try:
@@ -27,13 +28,11 @@ def takeorder(request):
             #print 'Raw Data: "%s' %request.body
             print ('Raw Data: ' + request.body)
             parsed_json = request.body
-            # should be equivalent to:
-            # parsed_json = {"1": 3, "2": 6, "3": 1}
-            parsed_json["1"] = 3
-            # or
-            #for key, value in parsed_json:
+            for key, value in parsed_json:
                 # key is the primary key of the menu item
+                Order.object.create(order_contents=key)
                 # value is quantity ordered
+                Order.object.create(order_contents=value)
         return HttpResponse("OK")
 
 
