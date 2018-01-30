5c09a103fdd49aeafe3125bbee24d08829d33c67
25640857+kkablan <25640857+kkablan@users.noreply.github.com>
2018-01-30 21:41:17 +0000

    Updated methods from waiter/views to the current methods within the master branch.
    Updated old field names with the new ones from waiter/models.py

--- server/waiter/views.py
+++ server/waiter/views.py
@@ -5,116 +5,60 @@ import json
 from django.utils import timezone
 from django.shortcuts import render
 from django.core.serializers import serialize
+from django.views.decorators.http import require_http_methods
 
 
 def index(request):
-    return HttpResponse("This page has the potential to be the best page ever... it's just not there yet")
+    """Return the waiter index page."""
+    return HttpResponse("Waiter index page, to be implemented.")
 
 
-def make_order(request):
-    if request.is_ajax():
-        if request.method == 'POST':
-            all_orders = json.loads(request.body.decode('utf-8'))
-            print("Recieved order from front-end: ", all_orders)
-
-            order_contents = [Menu.objects.get(pk=key) for key in all_orders]
-            total_price = sum([item.price * all_orders[str(item.id)] for item in order_contents])
-            new_order = Order(
-                table="0",
-                complete=False,
-                time=timezone.now(),
-                items=", ".join([str(item) for item in order_contents]),
-                cooking_instructions='none',
-                purchase_method='none',
-                total_price=total_price,
-                delivered=False,
-                table_assistance=False)
-            new_order.save(force_insert=True)
-
-            return HttpResponse("recieved")
-    return HttpResponse("Post order JSON to this endpoint.")
-
-
-# used to confirm an order
-def confirm_order(request):
-    if request.is_ajax():
-        if request.method == 'POST':
-            order_id = json.loads(request.body.decode('utf-8'))["id"]
-            print("Recieved ID: " + str(order_id))
-            order = Order.objects.get(pk=order_id)
-            print(order)
-            order.complete = True
-            order.save()
-            print(Order.objects.values_list('complete'))
-            return HttpResponse("recieved")
-    return HttpResponse("Post orderID to confirm to this endpoint.")
-
-
-def order_status(request):
-    orderstatus = order_update()
-    orderstatus = {
-        'orderstatus': orderstatus,
-    }
-    # can use the readorders within the HTML in a template to access a list of ready orders
-    return HttpResponse(ready_orders)  # this line returns the http with the list of ready orders
-    # return HttpResponse()  # this line returns an empty page (the effects can be seen in the terminal)
-
-    # This method is used to check the db every 30 seconds
-    # to see if the order has updated.
-
-
-def ready_orders(request):
-    readyorders = order_update_ready_only()
-    readyorders = {
-        'readyorders': readyorders,
-    }
-    # can use the readorders within the HTML in a template to access a list of ready orders
-    return HttpResponse(readyorders)  # this line returns the http with the list of ready orders
-    # return HttpResponse()  # this line returns an empty page (the effects can be seen in the terminal)
-
-    # This method is used to check the db every 30 seconds
-    # to see if the order has updated.
-
-
-# page for displaying all orders
 def orders(request):
+    """Return the page for viewing all orders."""
     return render(request, "waiter/orders.html")
 
 
-# returns JSON of all orders from orders table
+@require_http_methods(["GET"])
 def get_orders(request):
-    orders_json = serialize('json', Order.objects.all())
-    return JsonResponse(orders_json, safe=False)
+    """Return all orders as JSON."""
+    json = serialize('json', Order.objects.all())
+    return JsonResponse(json, safe=False)
 
 
-def order_update_ready_only():
-    print ("-----checking for order update-----")
-    readyorders = Order.objects.filter(complete=True)
-    # readyorders = Order.objects.all
-    # print (Order.objects.all())
-    try:
-        for curIt in readyorders:
-            # Note -- add a method here to send the readyorders object i.e. through JSON.
-            print (str(curIt.id) + ' -> ' + 'READY ')  # str(curIt.orderStatus))
-    except:
-        print("error printing")
-    return readyorders
+@require_http_methods(["GET"])
+def ready_orders(request):
+    """Return all ready orders as JSON."""
+    json = serialize('json', Order.objects.filter(complete=True))
+    return JsonResponse(json, safe=False)
 
 
-def order_update():
-    print ("-----checking for order update-----")
-    readyorders = Order.objects.all()
-    realyreadyorders = Order.objects.filter(complete=True)
-    notreadyorders = Order.objects.filter(complete=False)
-    # readyorders = Order.objects.all
-    # print (Order.objects.all())
-    try:
-        for curIt in realyreadyorders:
-            # Note -- add a method here to send the readyorders object i.e. through JSON.
-            print (str(curIt.id) + ' -> ' + 'READY ')  # str(curIt.orderStatus))
-        for curIt in notreadyorders:
-            # Note -- add a method here to send the readyorders object i.e. through JSON.
-            print (str(curIt.id) + ' -> ' + 'NOT READY ')  # str(curIt.orderStatus))
-    except:
-        print("error printing")
-    return readyorders
+@require_http_methods(["POST"])
+def make_order(request):
+    """Create an order from the provided JSON."""
+    all_orders = json.loads(request.body.decode('utf-8'))
+    print("Recieved order: ", all_orders)
+    order_contents = [Menu.objects.get(pk=key) for key in all_orders]
+    total_price = sum([item.price * all_orders[str(item.id)] for item in order_contents])
+    new_order = Order(
+        table="0",
+        complete=False,
+        time=timezone.now(),
+        items=", ".join([str(item) for item in order_contents]),
+        cooking_instructions='none',
+        purchase_method='none',
+        total_price=total_price,
+        delivered=False,
+        table_assistance=False)
+    new_order.save(force_insert=True)
+    return HttpResponse("recieved")
+
+
+@require_http_methods(["POST"])
+def confirm_order(request):
+    """Confirm the provided order in the database."""
+    order_id = json.loads(request.body.decode('utf-8'))["id"]
+    print("Recieved ID: " + str(order_id))
+    order = Order.objects.get(pk=order_id)
+    order.complete = True
+    order.save()
+    return HttpResponse("recieved")
