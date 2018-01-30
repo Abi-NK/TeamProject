c15d0681462ca82e351edf88b8d3bd95101adcc3
H09bdavies <h09bryndavies@gmail.com>
2018-01-30 13:57:24 +0000

    Refactor waiter views.py
    Pair programmed with @ausanc, cleaned up views, removed redundant
    methods,

--- server/waiter/urls.py
+++ server/waiter/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('makeorder', views.make_order, name='makeorder'),
-    path('orderstatus', views.order_status, name='orderstatus'),
     path('readyorders', views.ready_orders, name='readyorders'),
     path('orders', views.orders, name='orders'),
     path('getorders', views.get_orders, name='getorders'),
--- server/waiter/views.py
+++ server/waiter/views.py
@@ -5,73 +5,47 @@ import json
 from django.utils import timezone
 from django.shortcuts import render
 from django.core.serializers import serialize
+from django.views.decorators.http import require_http_methods
 
 
 def index(request):
     return HttpResponse("This page has the potential to be the best page ever... it's just not there yet")
 
 
+@require_http_methods(["POST"])
 def make_order(request):
-    if request.is_ajax():
-        if request.method == 'POST':
-            all_orders = json.loads(request.body.decode('utf-8'))
-            print("Recieved order from front-end: ", all_orders)
+    all_orders = json.loads(request.body.decode('utf-8'))
+    print("Recieved order from front-end: ", all_orders)
 
-            order_contents = [Menu.objects.get(pk=key) for key in all_orders]
-            total_price = sum([item.price * all_orders[str(item.id)] for item in order_contents])
-            new_order = Order(
-                customer_name="none",
-                order_complete=False,
-                time_taken=timezone.now(),
-                order_contents=", ".join([str(item) for item in order_contents]),
-                cooking_instructions='none',
-                purchase_method='none',
-                total_price=total_price)
-            new_order.save(force_insert=True)
+    order_contents = [Menu.objects.get(pk=key) for key in all_orders]
+    total_price = sum([item.price * all_orders[str(item.id)] for item in order_contents])
+    new_order = Order(
+        customer_name="none",
+        order_complete=False,
+        time_taken=timezone.now(),
+        order_contents=", ".join([str(item) for item in order_contents]),
+        cooking_instructions='none',
+        purchase_method='none',
+        total_price=total_price)
+    new_order.save(force_insert=True)
 
-            return HttpResponse("recieved")
-    return HttpResponse("Post order JSON to this endpoint.")
+    return HttpResponse("recieved")
 
 
 # used to confirm an order
+@require_http_methods(["POST"])
 def confirm_order(request):
-    if request.is_ajax():
-        if request.method == 'POST':
-            order_id = json.loads(request.body.decode('utf-8'))["id"]
-            print("Recieved ID: " + str(order_id))
-            order = Order.objects.get(pk=order_id)
-            print(order)
-            order.order_complete = True
-            order.save()
-            print(Order.objects.values_list('order_complete'))
-            return HttpResponse("recieved")
-    return HttpResponse("Post orderID to confirm to this endpoint.")
-
-
-def order_status(request):
-    orderstatus = order_update()
-    orderstatus = {
-        'orderstatus': orderstatus,
-    }
-    # can use the readorders within the HTML in a template to access a list of ready orders
-    return HttpResponse(readyorders)  # this line returns the http with the list of ready orders
-    # return HttpResponse()  # this line returns an empty page (the effects can be seen in the terminal)
-
-    # This method is used to check the db every 30 seconds
-    # to see if the order has updated.
+    order_id = json.loads(request.body.decode('utf-8'))["id"]
+    print("Recieved ID: " + str(order_id))
+    order = Order.objects.get(pk=order_id)
+    order.order_complete = True
+    order.save()
+    return HttpResponse("recieved")
 
 
 def ready_orders(request):
-    readyorders = order_update_ready_only()
-    readyorders = {
-        'readyorders': readyorders,
-    }
-    # can use the readorders within the HTML in a template to access a list of ready orders
-    return HttpResponse(readyorders)  # this line returns the http with the list of ready orders
-    # return HttpResponse()  # this line returns an empty page (the effects can be seen in the terminal)
-
-    # This method is used to check the db every 30 seconds
-    # to see if the order has updated.
+    orders_json = serialize('json', Order.objects.filter(order_complete=True))
+    return JsonResponse(orders_json, safe=False)
 
 
 # page for displaying all orders
@@ -83,36 +57,3 @@ def orders(request):
 def get_orders(request):
     orders_json = serialize('json', Order.objects.all())
     return JsonResponse(orders_json, safe=False)
-
-
-def order_update_ready_only():
-    print ("-----checking for order update-----")
-    readyorders = Order.objects.filter(order_complete=True)
-    # readyorders = Order.objects.all
-    # print (Order.objects.all())
-    try:
-        for curIt in readyorders:
-            # Note -- add a method here to send the readyorders object i.e. through JSON.
-            print (str(curIt.id) + ' -> ' + 'READY ')  # str(curIt.orderStatus))
-    except:
-        print("error printing")
-    return readyorders
-
-
-def order_update():
-    print ("-----checking for order update-----")
-    readyorders = Order.objects.all()
-    realyreadyorders = Order.objects.filter(order_complete=True)
-    notreadyorders = Order.objects.filter(order_complete=False)
-    # readyorders = Order.objects.all
-    # print (Order.objects.all())
-    try:
-        for curIt in realyreadyorders:
-            # Note -- add a method here to send the readyorders object i.e. through JSON.
-            print (str(curIt.id) + ' -> ' + 'READY ')  # str(curIt.orderStatus))
-        for curIt in notreadyorders:
-            # Note -- add a method here to send the readyorders object i.e. through JSON.
-            print (str(curIt.id) + ' -> ' + 'NOT READY ')  # str(curIt.orderStatus))
-    except:
-        print("error printing")
-    return readyorders
