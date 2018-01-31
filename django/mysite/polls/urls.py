7449221df926a567fa6a8c509c7bf57fdc9e5f55
Rodney Olav C. Melby <16503898+RHUL-INFO-SEC-MASTERS@users.noreply.github.com>
2018-01-31 21:54:01 +0000

    Django Tutorial 2.0.1 Part 4

--- django/mysite/polls/urls.py
+++ django/mysite/polls/urls.py
@@ -2,14 +2,24 @@ from django.urls import path
 
 from . import views
 
-# Namespacing URL names
+# Namespacing and Generic Views in Django
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+## Without Generic Views ##
+# Namespacing URL names
+#app_name = 'polls'
+#urlpatterns = [
+#    path('', views.index, name='index'),
+#    path('<int:question_id>/', views.detail, name='detail'),
+#    path('<int:question_id>/results/', views.results, name='results'),
+#    path('<int:question_id>/vote/', views.vote, name='vote'),
+#]
 
 # Without Namespacing URL names
 #urlpatterns = [
--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,18 +1,63 @@
 # Django With RENDER and get object or 404 error
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+## Generic Views
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+## Without Generic Views
+#def index(request):
+#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    context = {'latest_question_list': latest_question_list}
+#    return render(request, 'polls/index.html', context)
 
 # get object or 404 error
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+
+# write results view
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+# Add forms
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 #from django.http import Http404
 # Standard django 404
@@ -55,9 +100,9 @@ def detail(request, question_id):
 #def detail(request, question_id):
 #    return HttpResponse("You're looking at question %s." % question_id)
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+#def results(request, question_id):
+#    response = "You're looking at the results of question %s."
+#    return HttpResponse(response % question_id)
 
-def vote(request, question_id):
-   return HttpResponse("You're voting on question %s." % question_id)
+#def vote(request, question_id):
+#   return HttpResponse("You're voting on question %s." % question_id)
