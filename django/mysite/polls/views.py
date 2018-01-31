da7ade2caec8f94a3cc3a7dfd0a420c1000e6db7
Rodney Olav C. Melby <16503898+RHUL-INFO-SEC-MASTERS@users.noreply.github.com>
2018-01-31 19:03:17 +0000

    Refactor: update polls/views.py to get object or 404 error.

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,6 +1,5 @@
-# Django With RENDER
-from django.http import Http404
-from django.shortcuts import render
+# Django With RENDER and get object or 404 error
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
 from .models import Question
@@ -10,16 +9,23 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
-# ...
+# get object or 404 error
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+
+#from django.http import Http404
+# Standard django 404
+#def detail(request, question_id):
+#    try:
+#        question = Question.objects.get(pk=question_id)
+#    except Question.DoesNotExist:
+#        raise Http404("Question does not exist")
+#    return render(request, 'polls/detail.html', {'question': question})
 
 # Django No RENDER
 
+#from django.http import Http404
 #from django.shortcuts import render
 #from django.http import HttpResponse
 #from django.template import loader
