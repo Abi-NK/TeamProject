d2f133764ab6b579fbd75afcb8e8788640cbc599
Rodney Olav C. Melby <16503898+RHUL-INFO-SEC-MASTERS@users.noreply.github.com>
2018-01-31 18:33:27 +0000

    Refactor: update polls/views.py to use index view template.

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
@@ -8,8 +8,16 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    # Django No template
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    #return HttpResponse(output)
+  
+    #Django with template
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 # Leave the rest of the views (detail, results, vote) unchanged
 
