1614e510d4dfd2a44eadf549170112d6adaa1533
Rodney Olav C. Melby <16503898+RHUL-INFO-SEC-MASTERS@users.noreply.github.com>
2018-01-31 18:26:35 +0000

    Core: Add index.html template for views.

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
 from django.http import HttpResponse
+
+from .models import Question
+
+# Create your views here.
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+# Leave the rest of the views (detail, results, vote) unchanged
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
