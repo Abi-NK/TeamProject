bc46430d1649f5895ee416b8ec6b23570fdabe11
Rodney Olav C. Melby <16503898+RHUL-INFO-SEC-MASTERS@users.noreply.github.com>
2018-02-01 23:29:49 +0000

    Example: Django 2.0.1 Tutorial Part 5

--- django/mysite/polls/models.py
+++ django/mysite/polls/models.py
@@ -12,9 +12,14 @@ class Question(models.Model):
     # def to return string
     def __str__(self):
         return self.question_text
-    # def to return time of publish
+    ## Tutorial Part5 bug removal
+    #def was_published_recently(self):
+    #    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+    # def to return time of publish    
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+      now = timezone.now()
+      return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
