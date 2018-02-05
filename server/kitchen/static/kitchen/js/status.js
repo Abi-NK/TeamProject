2264874df83819a0a30bcd236220f1fb66138d48
25640857+kkablan <25640857+kkablan@users.noreply.github.com>
2018-02-05 12:42:41 +0000

    Fixed kitchen UI Added time parser Added date parser Adding colour function

--- server/kitchen/static/kitchen/js/status.js
+++ server/kitchen/static/kitchen/js/status.js
@@ -23,7 +23,7 @@ function makeOrderHTML(orderID, items, time, cooking_instructions, confirmed, de
   <div class="col-md-12 mb-md-3">
     <div class="card">
       <div id="body" class="card-body">
-      <script>changeColour(this.time);</script>
+        <script>${changeColour(time, this)} </script>
         <div class="row">
           <div class="col-md-9">
             <h2 class="card-title">Order #${ orderID }</h2>
@@ -31,7 +31,7 @@ function makeOrderHTML(orderID, items, time, cooking_instructions, confirmed, de
                 <b>Items:</b> <br> ${ items }
             </p>
             <p>
-                <b>Time:</b> &nbsp; ${ parseTime(time) }
+                <b>Time:</b> &nbsp; ${ parseTime(time, 'H') +":" +  parseTime(time, 'M') + ":"+ parseTime(time, 'S') }
             </p>
             <p>
                 <b>Cooking instructions:</b> &nbsp; ${ cooking_instructions }
@@ -55,6 +55,8 @@ function makeOrderHTML(orderID, items, time, cooking_instructions, confirmed, de
   `;
 }
 
+// makes boolean vars easyer to read
+// returns eather yes or no strings depending on bool
 function easyRead(bool){
     if (bool){
         return "yes";
@@ -63,19 +65,49 @@ function easyRead(bool){
     }
 }
 
-function parseTime(time){
-    return time
+// this function returns the current time --> needs fixing
+function timeNow(){
+    var d = new Date();
+    d = d.getTime();
+    return d;
 }
 
-function changeColour(time) {
-    ////document.getElementById("body").style.backgroundColor = "powderblue";
+// time parser that takes argument of frame
+// returns houres, mins or secs depengin on frame
+function parseTime(time, frame){
+    var timeString = time;
+
+    if (frame == 'H'){
+        var timeOnly = timeString.slice(11, 13);
+    } else if (frame == 'M'){
+        var timeOnly = timeString.slice(14, 16);
+    } else if (frame == 'S'){
+        var timeOnly = timeString.slice(17, 19);
+    }
+    return timeOnly;
+}
+
+// returns date
+function parseDate(time){
+    var timeString = time;
+    var DateOnly = timeString.slice(0, 9);
+    return DateOnly;
+}
+
+//changes color depenign on the age of the order
+function changeColour(time, id) {
 
     var d = new Date();
+    d = d.getTime();
 
-    if(time-10 > d.getTime()){
-        document.getElementById("body").style.backgroundColor = "RED";
+    var orderTimeM = parseTime(time, 'M');
+
+    if((orderTimeM - 10) > d){
+        document.getElementsByClassName("card-body").style.backgroundColor = "RED";
     }else{
-        document.getElementById("body").style.backgroundColor = "LIGHTGREEN";
+        //document.getElementById("body").style.backgroundColor = "GREEN";
+        //document.getElementsByClassName("card-body").style.backgroundColor = "RED";
+        document.getElementById(id).style.backgroundColor = "GREEN";
     }
 }
 
@@ -96,7 +128,7 @@ function updateOrders(){
   });
 }
 
-function setReadyDelivery(button, orderID){
+function setReadyDelivery(button, orderID){ //add check for confirmed first
   $.ajax({
     url: "/kitchen/readyDelivery",
     type: 'POST',
