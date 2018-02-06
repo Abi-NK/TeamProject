a20ecfa8bdc47cb231c28f82bb02747a6f36f8e1
Ausanc <henrygilbert15@live.co.uk>
2018-02-06 13:19:49 +0000

    Add a list of seating with booleans showing if they are available

--- server/customer/views.py
+++ server/customer/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 from .models import Menu
 from django.views.decorators.csrf import ensure_csrf_cookie
 
+number_of_tables = 20
+seating = [True] * number_of_tables
+print([str(i) for i, seat in enumerate(seating) if seating[i]])
+
 
 @ensure_csrf_cookie
 def index(request):
     """Return the menu page."""
-    return render(request, 'customer/menu.html', {'all_menu': Menu.objects.all()})
+    return render(request, 'customer/menu.html', {
+        'all_menu': Menu.objects.all(),
+        'seating': [str(i) for i, seat in enumerate(seating) if seating[i]]
+    })
 
 
 def detail(request, menu_id):
