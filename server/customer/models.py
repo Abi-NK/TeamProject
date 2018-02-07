2bca832422dd07480b15590f1cb7d78367070019
Ausanc <henrygilbert15@live.co.uk>
2018-02-07 12:26:01 +0000

    Add Seating model
    Add Seating model for traking tables in the restaurant
    Add buildseating command to populate Seating with sample seating
    Add migrations for Seating
    Rebuild database with new model and repopulate with sample data

--- server/customer/models.py
+++ server/customer/models.py
@@ -9,8 +9,16 @@ class Menu(models.Model):
     description = models.CharField(max_length=1000)
     course = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
-    allergy = models.CharField(max_length=1000)
-    calories = models.IntegerField()
+    allergy = models.CharField(max_length=1000, default="")
+    calories = models.IntegerField(default=0)
 
     def __str__(self):
         return "%s (%s)" % (self.name, self.course)
+
+
+class Seating(models.Model):
+    label = models.CharField(max_length=25, default='Table 0')
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.label
