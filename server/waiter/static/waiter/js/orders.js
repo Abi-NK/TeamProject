1e36e4faa4cfb1af1aacc004a70da98cc3139d3e
H09bdavies <h09bryndavies@gmail.com>
2018-02-08 12:26:38 +0000

    Refactor time to be in a readable format

--- server/waiter/static/waiter/js/orders.js
+++ server/waiter/static/waiter/js/orders.js
@@ -34,7 +34,7 @@ function makeOrderHTML(orderID, tableLabel, contents, price, confirmed, time){
               ${ contents }
             </p>
             <h3>Total price: ${ toPrice(price) }
-			<h3>Time Ordered: ${ (time) }
+			<h3>Time Ordered: ${ (time.substring(11,19)) }
           </div>
           <div class="col-md-3 text-center">
             <button type="button" class="btn btn-primary btn-lg"
