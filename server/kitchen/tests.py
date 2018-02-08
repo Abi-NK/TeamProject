5e60bf6b8f8d604269dba2a78033fc549ecb522a
25640857+kkablan <25640857+kkablan@users.noreply.github.com>
2018-02-08 21:50:00 +0000

    Added testing kitchen ready_delivery has a testable function

--- server/kitchen/tests.py
+++ server/kitchen/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from waiter.models import Order
+from django.utils import timezone
+from kitchen.views import readyDelivery
 
-# Create your tests here.
+
+class TestMakingDelivery(TestCase):
+
+    def setUp(self):
+        Order.objects.create(pk=666,
+                             table="Test Table",
+                             items="Test item 1",
+                             time=timezone.now(),
+                             cooking_instructions="none",
+                             purchase_method="none",
+                             total_price=66.60,
+                             confirmed=False,
+                             ready_delivery=False,
+                             delivered=False)
+
+    def test_ready_delivery(self):
+        """Orders that are ready for delivery"""
+        test_order = Order.objects.get(pk=666)
+        #readyDelivery(str(test_order.id))
+        self.assertEqual(readyDelivery(str(test_order.id)), True)
