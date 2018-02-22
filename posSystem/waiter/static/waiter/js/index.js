1e0fe67c5816d2755c4e7365c9297cd7dbb3be05
Ausanc <henrygilbert15@live.co.uk>
2018-02-22 17:04:44 +0000

    Refactor waiter/orders into a column on waiter/index

--- posSystem/waiter/static/waiter/js/index.js
+++ posSystem/waiter/static/waiter/js/index.js
@@ -10,3 +10,29 @@ function requestHelp(id){
     }
   });
 }
+
+function updateOrders(){
+  $.get("getorders", function(data){
+    $("#container-confirm").html(data);
+  });
+}
+
+function confirmOrder(button, orderID){
+  $.ajax({
+    url: "/waiter/confirmorder",
+    type: 'POST',
+    headers: {'X-CSRFToken': csrfToken},
+    contentType: 'application/json; charset=utf-8',
+    data: JSON.stringify({id: orderID}),
+    dataType: 'text',
+    success: function(result) {
+      $(button).attr("disabled", true);
+      $(button).removeClass("btn-primary").addClass("btn-success")
+      $(button).text("Confirmed");
+    }
+  });
+}
+
+$(document).ready(function(){
+  updateOrders();
+});
--- posSystem/waiter/urls.py
+++ posSystem/waiter/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('makeorder', views.make_order, name='makeorder'),
     path('readyorders', views.ready_orders, name='readyorders'),
-    path('orders', views.orders, name='orders'),
     path('getorders', views.get_orders, name='getorders'),
     path('confirmorder', views.confirm_order, name='confirmorder'),
     path('requesthelp', views.request_help, name='requesthelp'),
