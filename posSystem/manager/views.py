d33ff8347256337b1924db14a0d58be496ade727
AbhishekNk1 <zdac041@live.rhul.ac.uk>
2018-03-08 16:11:01 +0000

    Fixed an error where you didn't need validation to update an item.

--- posSystem/manager/views.py
+++ posSystem/manager/views.py
@@ -84,8 +84,8 @@ def adjust_menu(request):
                                     'stock': request.POST['menu_stock']})
 
         # if the confirm change button was pressed, check form for validation and update menu
-        if 'confirm' in request.POST:
-            if form.is_valid():
+        if form.is_valid():
+            if 'confirm' in request.POST:
                 print("item changed")
                 menu_update = Menu.objects.get(pk=request.POST['menu_id'])
                 menu_update.name = request.POST['menu_name']
@@ -102,17 +102,17 @@ def adjust_menu(request):
                 menu_update.stock = request.POST['menu_stock']
                 menu_update.save()
 
-        # if the delete button was pressed, remove the item from menu
-        elif 'delete' in request.POST:
-            Menu.objects.filter(pk=request.POST['menu_id']).delete()
+            # if the delete button was pressed, remove the item from menu
+            elif 'delete' in request.POST:
+                Menu.objects.filter(pk=request.POST['menu_id']).delete()
 
-        elif 'add_item' in request.POST:
-            Menu.objects.create(name=request.POST['menu_name'], price=request.POST['menu_price'],
-                                description=request.POST['menu_description'], course=request.POST['menu_course'],
-                                category=request.POST['menu_category'], allergy=request.POST['menu_allergy'],
-                                calories=request.POST['menu_calories'], image=request.POST['menu_image'],
-                                vegetarian=request.POST['menu_vegetarian'], vegan=request.POST['menu_vegan'],
-                                meat=request.POST['menu_meat'], stock=request.POST['menu_stock'])
+            elif 'add_item' in request.POST:
+                Menu.objects.create(name=request.POST['menu_name'], price=request.POST['menu_price'],
+                                    description=request.POST['menu_description'], course=request.POST['menu_course'],
+                                    category=request.POST['menu_category'], allergy=request.POST['menu_allergy'],
+                                    calories=request.POST['menu_calories'], image=request.POST['menu_image'],
+                                    vegetarian=request.POST['menu_vegetarian'], vegan=request.POST['menu_vegan'],
+                                    meat=request.POST['menu_meat'], stock=request.POST['menu_stock'])
 
     context = {"menu": Menu.objects.all()}
     return render(request, 'manager/managermenu.html', context)
