271f90ed04739041fd5419a35ab887bff9752c50
AbhishekNk1 <zdac041@live.rhul.ac.uk>
2018-03-08 15:37:22 +0000

    Adjusted the manager adjust menu options and unit tests to take into account the new Menu table fields added to master.

--- posSystem/manager/views.py
+++ posSystem/manager/views.py
@@ -74,12 +74,14 @@ def adjust_menu(request):
     """Return the menu in formatted HTML and update the table based on inputs."""
     if request.method == "POST":
 
-        # check if form inputs are valid then send it to database
+        # VALIDATION: check if form inputs are valid then send it to database
         form = AdjustMenuForm(data={'name': request.POST['menu_name'], 'price': request.POST['menu_price'],
                                     'description': request.POST['menu_description'],
                                     'course': request.POST['menu_course'], 'category': request.POST['menu_category'],
                                     'allergy': request.POST['menu_allergy'], 'calories': request.POST['menu_calories'],
-                                    'image': request.POST['menu_image']})
+                                    'image': request.POST['menu_image'], 'vegetarian': request.POST['menu_vegetarian'],
+                                    'vegan': request.POST['menu_vegan'], 'meat': request.POST['menu_meat'],
+                                    'stock': request.POST['menu_stock']})
 
         # if the confirm change button was pressed, check form for validation and update menu
         if 'confirm' in request.POST:
@@ -94,6 +96,10 @@ def adjust_menu(request):
                 menu_update.allergy = request.POST['menu_allergy']
                 menu_update.calories = request.POST['menu_calories']
                 menu_update.image = request.POST['menu_image']
+                menu_update.vegetarian = request.POST['menu_vegetarian']
+                menu_update.vegan = request.POST['menu_vegan']
+                menu_update.meat = request.POST['menu_meat']
+                menu_update.stock = request.POST['menu_stock']
                 menu_update.save()
 
         # if the delete button was pressed, remove the item from menu
@@ -104,7 +110,9 @@ def adjust_menu(request):
             Menu.objects.create(name=request.POST['menu_name'], price=request.POST['menu_price'],
                                 description=request.POST['menu_description'], course=request.POST['menu_course'],
                                 category=request.POST['menu_category'], allergy=request.POST['menu_allergy'],
-                                calories=request.POST['menu_calories'], image=request.POST['menu_image'])
+                                calories=request.POST['menu_calories'], image=request.POST['menu_image'],
+                                vegetarian=request.POST['menu_vegetarian'], vegan=request.POST['menu_vegan'],
+                                meat=request.POST['menu_meat'], stock=request.POST['menu_stock'])
 
-    context = {"menu": Menu.objects.all(),}
+    context = {"menu": Menu.objects.all()}
     return render(request, 'manager/managermenu.html', context)
