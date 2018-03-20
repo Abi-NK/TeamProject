c8e02f47e0baa5ee8d9ec168131f6e96186b954b
AbhishekNk1 <zdac041@live.rhul.ac.uk>
2018-03-20 17:48:09 +0000

    Updated some tests in test_manager_menu.py and fixed some PEP8 errors in menu_views.

--- posSystem/core/views/menu_views.py
+++ posSystem/core/views/menu_views.py
@@ -8,7 +8,7 @@ import json
 
 
 def adjust_menu(request):
-    """Return the menu in formatted HTML and update the table based on inputs."""
+    """Return the menu in formatted HTML and update the table based on inputs by the manager."""
     if request.method == "POST":
 
         # VALIDATION: check if form inputs are valid then send it to database
@@ -25,20 +25,13 @@ def adjust_menu(request):
         if form.is_valid():
             if 'confirm' in request.POST:
                 print("item changed")
-                menu_update = Menu.objects.get(pk=request.POST['menu_id'])
-                menu_update.name = request.POST['menu_name']
-                menu_update.price = request.POST['menu_price']
-                menu_update.description = request.POST['menu_description']
-                menu_update.course = request.POST['menu_course']
-                menu_update.category = request.POST['menu_category']
-                menu_update.allergy = request.POST['menu_allergy']
-                menu_update.calories = request.POST['menu_calories']
-                menu_update.image = request.POST['menu_image']
-                menu_update.vegetarian = request.POST['menu_vegetarian']
-                menu_update.vegan = request.POST['menu_vegan']
-                menu_update.meat = request.POST['menu_meat']
-                menu_update.stock = request.POST['menu_stock']
-                menu_update.save()
+                Menu.objects.filter(pk=request.POST['menu_id']).update(
+                    name=request.POST['menu_name'], price=request.POST['menu_price'],
+                    description=request.POST['menu_description'], course=request.POST['menu_course'],
+                    category=request.POST['menu_category'], allergy=request.POST['menu_allergy'],
+                    calories=request.POST['menu_calories'], image=request.POST['menu_image'],
+                    vegetarian=request.POST['menu_vegetarian'], vegan=request.POST['menu_vegan'],
+                    meat=request.POST['menu_meat'], stock=request.POST['menu_stock'])
 
             # if the delete button was pressed, remove the item from menu
             elif 'delete' in request.POST:
@@ -58,11 +51,11 @@ def adjust_menu(request):
 
 @require_http_methods(["POST"])
 def remove_menu_item(request):
-    '''No idea what to do here'''
+    """Return formatted HTML to remove a menu item"""
     received_json = json.loads(request.body.decode('utf-8'))
-    itemToRemoveID = received_json["itemToRemoveID"]
-    menu_item = Menu.objects.get(pk=itemToRemoveID)
-    if (menu_item.removed):
+    item_to_remove_id = received_json["itemToRemoveID"]
+    menu_item = Menu.objects.get(pk=item_to_remove_id)
+    if menu_item.removed:
         menu_item.removed = False
     else:
         menu_item.removed = True
