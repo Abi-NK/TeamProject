f4ccd35dcb5a72942a7a1f537035250ac1e6642f
Ausanc <henrygilbert15@live.co.uk>
2018-03-20 09:27:30 +0000

    Begin fixing all the broken URLs

--- posSystem/core/urls.py
+++ posSystem/core/urls.py
@@ -1,40 +1,56 @@
 from django.urls import include, path
 from . import views
 
-urlpatterns = [
-    # Menu model urls
+menu_model_urls = [
     path('managermenu', views.adjust_menu, name='managermenu'),
     path('removemenuitem', views.remove_menu_item, name='removemenuitem'),
+]
 
-    # Order model urls
+order_model_urls = [
     path('makeorder', views.make_order, name='makeorder'),
     path('confirmorder', views.confirm_order, name='confirmorder'),
     path('cancelorder', views.cancel_order, name='cancelorder'),
     path('readyDelivery', views.readyDelivery, name='readyDelivery'),
     path('delayorder', views.delay_order, name='delayorder'),
     path('statuses', views.statuses, name='statuses'),
+]
 
-    # OrderExtra model urls
+orderextra_model_urls = [
     path('placeorderextra', views.place_order_extra, name='placeorderextra'),
     path('getorderextra', views.get_order_extra, name='getorderextra'),
     path('cancelorderextraitem', views.cancel_order_extra_item, name='cancelorderextraitem'),
+]
 
-    # OrderItem model urls
+orderitem_model_urls = [
 
-    # Payment model urls
+]
+
+payment_model_urls = [
     path('confirmPayment', views.confirm_payment, name='confirmPayment'),
+]
 
-    # Seating model urls
+seating_model_urls = [
     path('takeseat', views.take_seat, name='takeseat'),
     path('assigntoseating', views.assign_to_seating, name='assigntoseating'),
     path('unassignfromseating', views.unassign_from_seating, name='unassignfromseating'),
     path('requesthelp', views.request_help, name='requesthelp'),
     path('cancelhelp', views.cancel_help, name='cancelhelp'),
+]
 
-    # Waiter model urls
+waiter_model_urls = [
     path('waiteronduty', views.waiter_on_duty, name='waiteronduty'),
     path('waiteroffduty', views.waiter_off_duty, name='waiteroffduty'),
     path('getassignments', views.get_assignments, name='getassignments'),
     path('getwaiters', views.get_waiters, name='getwaiters'),
     path('autoassign', views.auto_assign, name='autoassign'),
+]
+
+urlpatterns = [
+    path('menu/', include(menu_model_urls)),
+    path('order/', include(order_model_urls)),
+    path('orderextra/', include(orderextra_model_urls)),
+    path('orderitem/', include(orderitem_model_urls)),
+    path('payment/', include(payment_model_urls)),
+    path('seating/', include(seating_model_urls)),
+    path('waiter/', include(waiter_model_urls)),
 ]
--- posSystem/customer/static/customer/js/menu.js
+++ posSystem/customer/static/customer/js/menu.js
@@ -90,7 +90,7 @@ function placeOrder(){
     console.log("Not placing order: order is empty.");
   } else {
     $.ajax({
-      url: "/waiter/makeorder",
+      url: "/core/order/makeorder",
       type: 'POST',
       headers: {'X-CSRFToken': csrfToken},
       contentType: 'application/json; charset=utf-8',
@@ -105,7 +105,7 @@ function placeOrder(){
 
 function requestHelp(){
   $.ajax({
-    url: "/waiter/requesthelp",
+    url: "/core/seating/requesthelp",
     type: 'POST',
     headers: {'X-CSRFToken': csrfToken},
     contentType: 'application/json; charset=utf-8',
@@ -132,7 +132,7 @@ $('.btn-seating-option').on('click', function(){
   // sends the selected seating ID to the server so it can be marked as taken, and stored in the user's session
   $('#seating-label').text(seatingLabel);
   $.ajax({
-    url: "/customer/takeseat",
+    url: "/core/seating/takeseat",
     type: 'POST',
     headers: {'X-CSRFToken': csrfToken},
     contentType: 'application/json; charset=utf-8',
@@ -170,7 +170,7 @@ function btnOrderExtraRemoveItem(button, order_extra_id, order_item_id){
   console.log("OrderExtra ID: " + order_extra_id);
   console.log("OrderItem ID: " + order_item_id);
   $.ajax({
-    url: "/customer/cancelorderextraitem",
+    url: "/core/orderextra/cancelorderextraitem",
     type: 'POST',
     headers: {'X-CSRFToken': csrfToken},
     contentType: 'application/json; charset=utf-8',
