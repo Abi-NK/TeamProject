ad6c856b11267fd8de12da358e73b4e24f9dad36
Tom McGregor <zdac099@live.rhul.ac.uk>
2018-03-22 15:46:04 +0000

    Add Pydoc for customer/views.py

--- posSystem/customer/views.py
+++ posSystem/customer/views.py
@@ -1,3 +1,10 @@
+'''
+
+Views for the customer section of the system. Views take a web request and return a web response.
+
+'''
+
+
 from django.shortcuts import render, redirect
 from core.models import Menu, Order, Payment, Seating
 from django.views.decorators.csrf import ensure_csrf_cookie
@@ -5,7 +12,13 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 
 @ensure_csrf_cookie
 def index(request):
-    """Return the menu page."""
+    """
+    Sets up the customer index page with menu displayed.
+
+    :param request: HTTPrequest
+    :return: HTTPresponse
+            Returns the customer menu html page
+    """
 
     if 'seating_label' in request.session:
 
@@ -29,7 +42,15 @@ def index(request):
 
 
 def payment(request):
-    """Either sends out a list of payment and order objects or takes in a payment using POST."""
+    """
+    Either sends out a list of payment and order objects or takes in a payment using POST.
+
+    :param request: HTTPrequest
+    :returns: HTTPresponse
+              Returns the customer index page if the request method is POST
+    :returns: HTTPresponse
+             Returns the electronic payment html page if the request method is not POST
+    """
     context = {
             'payment': Payment.objects.filter(order=request.session['seating_id']),
             'order': Order.objects.filter(table=request.session['seating_id']),
@@ -61,14 +82,28 @@ def payment(request):
 
 
 def checkbox_check(val):
-    """Converts html checkbox to django model format"""
+    """
+    Converts html checkbox to django model format
+
+    :param val: HTMLcheckbox
+                An html checkbox to be converted
+    :returns: Boolean
+              True if val is checked
+    :returns: Boolean
+              False if val is not checked
+    """
     if val == 'on':
         return "True"
     return "False"
 
 
 def t_and_c(request):
-    """Sets t and c field in DB from POST (not in html form)"""
+    """
+    Sets t and c field in DB from POST (not in html form)
+
+    :param request: HTTPrequest
+
+    """
     if request.method == "POST":
         payment_update = Payment.objects.get(terms_conditions=request.POST['t-c'])
         payment_update.delivered = True
@@ -76,6 +111,11 @@ def t_and_c(request):
 
 
 def statuses(request):
-    """..."""
+    """
+    Makes all orders available to the statuses page
+
+    :param request: HTTPrequest
+
+    """
     orders = Order.objects.all()
     return render(request, 'customer/statuses.html', {'orders': orders})
