cf84d55f1c173e8cd90bee18bd212fcdf6872615
Tom McGregor <zdac099@live.rhul.ac.uk>
2018-03-23 11:26:05 +0000

    Add more pydoc

--- posSystem/core/views/order_views.py
+++ posSystem/core/views/order_views.py
@@ -74,7 +74,7 @@ def readyDelivery(request):
 
     :param request: HTTPrequest
     :return: HTTPresponse
-             The received request notification of order ready.
+             The response notification of order ready.
     """
     order_id = json.loads(request.body.decode('utf-8'))["id"]
     Order.objects.get(pk=order_id).set_ready_delivery()
@@ -82,7 +82,14 @@ def readyDelivery(request):
 
 
 def delay_order(request):
-    """Delay the order."""
+    """
+
+    Delay the order.
+
+    :param request: HTTPrequest
+    :return: HTTPresponse
+             The response notification for a delayed order.
+    """
     order_id = json.loads(request.body.decode('utf-8'))["id"]
     print("Recieved ID: " + str(order_id))
     order = Order.objects.get(pk=order_id)
@@ -95,32 +102,67 @@ def delay_order(request):
 
 
 def html_kitchen_cards(request):
-    """Return all orders for the kitchen as formatted HTML."""
+    """
+
+     All orders for the kitchen as formatted HTML.
+
+    :param request: HTTPrequest
+    :return: HTTPresponse
+             All of the formatted kitchen orders.
+    """
     orders = Order.get_kitchen_orders(all)
     return render(request, "core/order/kitchen_cards.html", {'orders': orders})
 
 
 def html_confirm_cards(request):
-    """Return all orders which need confirmation as formatted HTML."""
+    """
+
+    All orders which need confirmation as formatted HTML
+
+    :param request: HTTPrequest
+    :return: HTTPresponse
+             The formatted non-confirmed orders.
+    """
     orders = Order.objects.filter(confirmed=False, cancelled=False)
     return render(request, "core/order/order_cards.html", {'orders': orders, 'confirm': True})
 
 
 def html_delivery_cards(request):
-    """Return all orders which are ready to be delivered as formatted HTML."""
+    """
+
+    All orders which are ready to be delivered as formatted HTML.
+
+    :param request: HTTPrequest
+    :return: HTTPresponse
+             The formatted ready to be delivered orders.
+    """
     orders = Order.objects.filter(confirmed=True, ready_delivery=True, delivered=False)
     return render(request, "core/order/order_cards.html", {'orders': orders, 'delivery': True})
 
 
 def html_unpaid_cards(request):  # what the user does not see, will not hurt them...
-    """Return all orders which have been delivered but not paid for as formatted HTML."""
+    """
+
+    All orders which have been delivered but not paid for as formatted HTML.
+
+    :param request: HTTPrequest
+    :return: HTTPresponse
+             The formatted HTML of all orders delivered but not paid for.
+    """
     # the order quiey bellow checks both the order model and the payment fk model
     orders = Order.unpaid_objects.filter(delivered=True).order_by('time')
     return render(request, "core/order/order_cards.html", {'orders': orders, 'unpaid': True})
 
 
 def html_summary_list(request):
-    """Return a summary of restaurant data in formatted HTML."""
+    """
+
+    Summary of restaurant data in formatted HTML.
+
+    :param request: HTTPrequest
+    :return: HTTPresponse
+             The formatted HTML of restaurant data.
+    """
     context = {
         "seating_data": {
             "occupied_count": len(Seating.occupied_objects.all()),
@@ -141,7 +183,14 @@ def html_summary_list(request):
 
 
 def html_active_list(request):
-    """Return all active orders in formatted HTML."""
+    """
+
+    All active orders in formatted HTML.
+
+    :param request: HTTPrequest
+    :return: HTTPresponse
+             The active orders.
+    """
     context = {
         "orders": Order.active_objects.all(),
     }
@@ -149,6 +198,14 @@ def html_active_list(request):
 
 
 def html_customer_cards(request):
+    """
+
+    The cards that display the orders to the waiter.
+
+    :param request: HTTPrequest
+    :return: HTTPresponse
+             The active and unpaid orders for a given table.
+    """
     unpaid_orders = Order.unpaid_objects.filter(table=request.session['seating_id'])
     active_orders = Order.active_objects.filter(table=request.session['seating_id'])
     orders = []
@@ -165,5 +222,13 @@ def html_customer_cards(request):
 
 
 def html_unpaid_dropdown(request):
+    """
+
+    A drop down of the unpaid orders.
+
+    :param request: HTTPrequest
+    :return: HTTPresponse
+             The orders that are unpaid.
+    """
     orders = Order.unpaid_objects.all()
     return render(request, 'core/order/unpaid_dropdown.html', {'orders': orders})
